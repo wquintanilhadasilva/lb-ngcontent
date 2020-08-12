dca56f1dca3f071e805839a876044230deb6d3e7
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2020-08-12 12:03:43 -0300

    add news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,17 @@ export class AppComponent {
 
   itens: Item[] = [
     {checked: false, description: 'Lorem Ipsum...', title: 'Item 1'},
-    {checked: true, description: 'Lorem Ipsum...', title: 'Item 2'},
-    {checked: false, description: 'Lorem Ipsum...', title: 'Item 3'},
-    {checked: true, description: 'Lorem Ipsum...', title: 'Item 4'},
+    {checked: true, description: 'Dolor consectetur...', title: 'Item 2'},
+    {checked: false, description: 'Sit Armet ...', title: 'Item 3'},
+    {checked: true, description: 'Adipisicing elit et dolor...', title: 'Item 4'},
   ];
+
+  acao2(val): void {
+    console.log(`acão 2 ${val}`);
+  }
+
+  acao1(val): void {
+    console.log(`acão 1 ${val}`);
+  }
 
 }
